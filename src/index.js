07080de09f2b0e9ecd01a56db89eb39400cde781
Aniket <aniketraj28042003@gmail.com>
2024-02-10 10:10:01 +0530

    added notification ticket model

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
-const { sendBasicEmail } = require("./services/email-service");
+// const { sendBasicEmail } = require("./services/email-service");
+const cron = require("node-cron");
 
 const setupAndStartServer = () => {
     const app = express();
@@ -13,11 +14,11 @@ const setupAndStartServer = () => {
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
 
-        sendBasicEmail(
-            "developer.aniket28@gmail.com",
-            "This is testing email",
-            "Hey, how are you, I hope you like the support"
-        )
+        // sendBasicEmail(
+        //     "developer.aniket28@gmail.com",
+        //     "This is testing email",
+        //     "Hey, how are you, I hope you like the support"
+        // )
     });
 
 }
