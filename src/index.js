fb1afa68c227c35f9d3695376da4cdcd708d5502
Aniket <aniketraj28042003@gmail.com>
2024-02-12 23:17:55 +0530

    added rabbitmq config

--- src/index.js
+++ src/index.js
@@ -2,32 +2,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
-const TicketService = require("./services/ticket-service");
-const job = require("./utils/job");
+// const TicketService = require("./services/ticket-service");
+// const job = require("./utils/job");
+const { createChannel } = require("./utils/messageQueues");
 const apiRoutes = require("./routes/index");
-const { EMAIL_ID, EMAIL_PASS } = require("./config/serverConfig");
 
-const ticketService = new TicketService();
-
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
     const app = express();
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
-    job();
+    // const channel = await createChannel();
+
+    // job();
 
     app.use("/api", apiRoutes);
 
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
-
-        // ticketService.sendBasicEmail(
-        //     "aniketraj28042003@gmail.com",
-        //     "aniketraj28042003@gmail.com",
-        //     "This is testing email",
-        //     "Hey, how are you, I hope you like the support"
-        // )
     });
 
 }
